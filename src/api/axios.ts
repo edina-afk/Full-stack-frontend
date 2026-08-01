923bf3f52941ca5b9e9609d3666dea8c4e46b22a
edina-afk <edenb928@gmail.com>
2026-08-01 01:48:14 -0700

    Save user data after login

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
- // baseURL: "https://full-stack-backend-bjvj.onrender.com",
+  baseURL: "https://full-stack-backend-bjvj.onrender.com",
 });
 
 export default api;
