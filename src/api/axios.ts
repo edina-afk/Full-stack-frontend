0d9d90d1d198c2d06291fb8718754f7757900447
edina-afk <edenb928@gmail.com>
2026-08-12 04:04:48 -0700

    Fix payment history and Ethiopian date

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,10 +1,22 @@
 import axios from "axios";
 
 const api = axios.create({
-      baseURL: "https://api.mensursultan.com",
-  headers:{
-    "Content-Type":"application/json"
+  baseURL: "https://api.mensursultan.com",
+});
+
+api.interceptors.request.use((config) => {
+  const storedUser = window.localStorage.getItem("user");
+
+  if (storedUser) {
+    const user = JSON.parse(storedUser);
+
+    if (user?.role) {
+      config.headers = config.headers || {};
+      config.headers["user-role"] = user.role;
+    }
   }
+
+  return config;
 });
 
 export default api;
